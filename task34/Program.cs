f18ed18763b60ee516a4c15ce7d92e0e48972650
Anastasiia <kariakina.nastia@example.com>
2022-08-23 15:12:04 +0300

    add Task 34

---
+++ task34/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] GetArray(int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(100, 999);
+int sumEven = 0;
+foreach (int i in array)
+{
+    if (i % 2 == 0)
+        sumEven += 1;
+}
+Console.WriteLine($"Сумма четных чисел: {sumEven}");

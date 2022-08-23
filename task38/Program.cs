c87f52cb04f8e0ab6934bc8caa25e22db6d776b9
Anastasiia <kariakina.nastia@example.com>
2022-08-23 15:34:53 +0300

    add Task 38

---
+++ task38/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+Console.WriteLine("Введите массив через запятую ");
+double[] array = Array.ConvertAll<string,double>
+(Console.ReadLine().Split(','), i => { return System.Convert.ToInt32 (i);});
+double maxValue = array[0];
+double minValue = array[0];
+int i = 0;
+int count = array.Length;
+while (i < count)
+{
+    if (array[i] >= maxValue)
+    {
+        maxValue = array[i];
+
+    }
+    else if (array[i] <= minValue)
+    {
+        minValue = array[i];
+    }
+i++;
+}
+double res = maxValue - minValue;
+Console.WriteLine(res);
